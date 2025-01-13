74043aaf00dbd502db5c61ae14811295a3bdb063
UnLuckyNikolay <nickslast@gmail.com>
2025-01-13 23:24:46 +0100

    Rewritten and cleaned up

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-# print ("hello world")
-
-file_path = "books/frankenstein.txt"
-with open(file_path) as f:
-    file_contents = f.read().lower()
-    amount_of_words = len(file_contents.split())
-#    print (amount_of_words)
-#    sorted_file_contents = re.sub('\W+', '', file_contents)
-    amount_of_characters = {}
-    for character in file_contents:
-        if character in 'abcdefghijklmnopqrstuvwxyz':
-            if character not in amount_of_characters:
-                amount_of_characters[character] = 1
-            else:
-                amount_of_characters[character] += 1
-#            if character == 'a' and amount_of_characters['a'] % 1000 == 0:
-#                print(f'amount of a is {amount_of_characters['a']}')
-    
+def main():
+    file_path = "books/frankenstein.txt"
+    with open(file_path) as f:
+        file_contents = f.read().lower()
+    amount_words = count_words(file_contents)
+    amount_characters = count_characters_dict(file_contents)
+   
     print(f"--- Report of {file_path} ---")
-    print(f"Amount of words: {amount_of_words}")
     print()
-    for word in sorted(amount_of_characters, key=amount_of_characters.get, reverse=True):
-        print(f"'{word}' - {amount_of_characters[word]}")
+    print(f"Amount of words: {amount_words}")
+    print()
+    for word in sorted(amount_characters, key=amount_characters.get, reverse=True):
+        print(f"'{word}' appeared {amount_characters[word]} times")
+    print()
     print("--- End of the report ---")
+
+def count_words(text):
+    amount = len(text.split())
+    return amount
+
+def count_characters_dict(text):
+    amount = {}
+    for character in text:
+        if character in 'abcdefghijklmnopqrstuvwxyz':
+            if character not in amount:
+                amount[character] = 1
+            else:
+                amount[character] += 1
+    return amount
+
+main()
